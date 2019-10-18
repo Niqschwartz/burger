0671187fe5f28134f3306942e47c2c2bced53305
Niq Schwartz <niqschwartz@gmail.com>
2019-10-18 15:08:27 -0500

    file structure comleted

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('turkey burger');
+INSERT INTO burgers (burger_name) VALUES ('bacon cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('bison burger');
