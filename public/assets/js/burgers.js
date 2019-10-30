4b5b184770bbfd90c0631bee2b2dfff45d17e8be
Niq Schwartz <niqschwartz@gmail.com>
2019-10-29 22:05:13 -0500

    fully functional

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -13,40 +13,40 @@ $(function() {
       })
     });
   
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
+  //   $(".create-form").on("submit", function(event) {
+  //     // Make sure to preventDefault on a submit event.
+  //     event.preventDefault();
   
-      var newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
-      };
+  //     var newCat = {
+  //       name: $("#ca").val().trim(),
+  //       sleepy: $("[name=sleepy]:checked").val().trim()
+  //     };
   
-      // Send the POST request.
-      $.ajax("/api/cats", {
-        type: "POST",
-        data: newCat
-      }).then(
-        function() {
-          console.log("created new cat");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+  //     // Send the POST request.
+  //     $.ajax("/api/cats", {
+  //       type: "POST",
+  //       data: newCat
+  //     }).then(
+  //       function() {
+  //         console.log("created new cat");
+  //         // Reload the page to get the updated list
+  //         location.reload();
+  //       }
+  //     );
+  //   });
   
-    $(".delete-cat").on("click", function(event) {
-      var id = $(this).data("id");
+  //   $(".delete-cat").on("click", function(event) {
+  //     var id = $(this).data("id");
   
-      // Send the DELETE request.
-      $.ajax("/api/cats/" + id, {
-        type: "DELETE"
-      }).then(
-        function() {
-          console.log("deleted cat", id);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
+  //     // Send the DELETE request.
+  //     $.ajax("/api/cats/" + id, {
+  //       type: "DELETE"
+  //     }).then(
+  //       function() {
+  //         console.log("deleted cat", id);
+  //         // Reload the page to get the updated list
+  //         location.reload();
+  //       }
+  //     );
+  //   });
   });
